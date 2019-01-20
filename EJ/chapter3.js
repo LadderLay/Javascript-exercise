8aa33bbb6df60cc78fa6961cf006f3bf4cfe178d
南瓜 <2509127422@qq.com>
2019-01-21 00:02:37 +0800

    feat: modify js

---
+++ EJ/chapter3.js
@@ -0,0 +1,34 @@
+// 3-1, Minimum
+function min(a, b) {
+  if(a > b)
+    return b;
+  else
+    return a;
+}
+
+// 3-2, Recursion
+function isEven(n) {
+  if(n === 0)
+    return true;
+  else if(n === 1)
+    return false;
+  else return isEven(n - 2);
+}
+
+// 3-3, Bean counting
+function countBs(str) {
+  let cnt = 0;
+  for(let i = 0; i < str.length; i++){
+    if(str.chartAt(i) === "B")
+      cnt++;
+  }
+  return cnt;
+}
+function countChar(str, ch) {
+  let cnt = 0;
+  for(let i = 0; i < str.length; i++){
+    if(str.chartAt(i) === ch)
+      cnt++;
+  }
+  return cnt;
+}
